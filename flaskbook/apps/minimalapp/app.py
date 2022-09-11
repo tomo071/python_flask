47c8cdb21d5cd22a23d7d5ff5ee5b753a0803418
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-09-12 08:18:35 +0900

    [add]httpメソッド/ルーティング

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -5,4 +5,10 @@ app=Flask(__name__)
 @app.route("/")
 def index():
   return "Hello Flaskbook!"
+
+@app.route("/hello/<name>",
+ methods=["GET","POST"],
+ endpoint="hello-endpoint")
+def hello(name):
+  return f"hello {name}!"
   
