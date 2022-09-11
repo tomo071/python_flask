00ec0617dc79b0f85ede3155192e737f3de5ae8f
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-09-12 08:31:45 +0900

    [add]htmlを使用した出力

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
@@ -11,4 +11,6 @@ def index():
  endpoint="hello-endpoint")
 def hello(name):
   return f"hello {name}!"
+def show_name(name):
+  return render_template("index.html", name=name)
   
