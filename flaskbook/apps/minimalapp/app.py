cf981f4036776c9daa3aacd75418a420bc1eae89
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-09-13 11:32:33 +0900

    [add]url_for

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app=Flask(__name__)
 
@@ -14,3 +14,7 @@ def hello(name):
 def show_name(name):
   return render_template("index.html", name=name)
   
+with app.test_request_context():
+  print(url_for("index"))
+  print(url_for("hello-endpoint", name="world"))
+  print(url_for("show_name", name="tomo", page="1"))
