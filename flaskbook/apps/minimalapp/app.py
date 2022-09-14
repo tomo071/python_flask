f340834f4ec7eb763515e7705fa5577ff9988a86
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-09-14 18:18:14 +0900

    [update]リクエストコンテキスト

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, current_app, g, request
 
 app=Flask(__name__)
 
@@ -18,3 +18,6 @@ with app.test_request_context():
   print(url_for("index"))
   print(url_for("hello-endpoint", name="world"))
   print(url_for("show_name", name="tomo", page="1"))
+
+with app.test_request_context("/uesrs?updated=true"):
+  print(request.args.get("updated"))
