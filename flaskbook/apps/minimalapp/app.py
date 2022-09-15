8f622f4a16323bdca66855f9d4cd196bf7ca7384
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-09-15 20:21:20 +0900

    [add]フォーム追加

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, current_app, g, request
+from flask import Flask, render_template, current_app, g, request, url_for, redirect
 
 app=Flask(__name__)
 
@@ -13,7 +13,19 @@ def hello(name):
   return f"hello {name}!"
 def show_name(name):
   return render_template("index.html", name=name)
-  
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")
+
+@app.eoute("/contact/complete",methods=["get","post"])
+def contact_complete():
+  if request.method == "post":
+
+    return redirect(url_for("contact_complete"))
+
+  return render_template("contact_complete.html")
+
 with app.test_request_context():
   print(url_for("index"))
   print(url_for("hello-endpoint", name="world"))
@@ -21,3 +33,4 @@ with app.test_request_context():
 
 with app.test_request_context("/uesrs?updated=true"):
   print(request.args.get("updated"))
+
